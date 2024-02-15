79445d71b7f91abcda8c8dafa29eff37ac91d9ac
Lý Mạnh Phi <lmanhphi01@gmail.com>
2024-02-15 21:24:09 +0700

    done lab6_1

--- Lab06/lab6_1/lib/main.dart
+++ Lab06/lab6_1/lib/main.dart
@@ -2,25 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: const MyApp(
+    const MaterialApp(
+      home: MyApp(
         maxScreens: 12,
+        currentScreen: 1,
       ),
     ),
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   final int maxScreens;
+  final int currentScreen;
 
-  const MyApp({Key? key, required this.maxScreens}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int currentScreen = 0;
+  const MyApp({Key? key, required this.maxScreens, required this.currentScreen})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +25,8 @@ class _MyAppState extends State<MyApp> {
         centerTitle: true,
         backgroundColor: Colors.blue,
         title: Text(
-          'Screen ${currentScreen+1}',
-          style: TextStyle(color: Colors.white),
+          'Screen $currentScreen',
+          style: const TextStyle(color: Colors.white),
         ),
       ),
       body: Stack(
@@ -40,17 +36,44 @@ class _MyAppState extends State<MyApp> {
             child: GridView.count(
               crossAxisCount: 3,
               children: [
-                for (int i = 1; i <= widget.maxScreens; i++)
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Container(
-                      height: 20,
-                      width: 50,
-                      color: Colors.blue,
-                      child: Center(
-                        child: Text(
-                          '$i',
-                          style: TextStyle(fontSize: 20, color: Colors.white),
+                for (int i = 1; i <= maxScreens; i++)
+                  GestureDetector(
+                    onTap: () {
+                      if (i < currentScreen) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyApp(
+                                maxScreens: maxScreens,
+                                currentScreen: i,
+                              ),
+                            )
+                        );
+                      } else if (i > currentScreen) {
+                        if (i - currentScreen <= 1) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyApp(
+                                maxScreens: maxScreens,
+                                currentScreen: i,
+                              ),
+                            ),
+                          );
+                        }
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Container(
+                        height: 20,
+                        width: 50,
+                        color: i <= currentScreen - 1 ? Colors.blue : Colors.grey,
+                        child: Center(
+                          child: Text(
+                            '$i',
+                            style: const TextStyle(fontSize: 20, color: Colors.white),
+                          ),
                         ),
                       ),
                     ),
@@ -58,30 +81,33 @@ class _MyAppState extends State<MyApp> {
               ],
             ),
           ),
-
           Positioned(
             bottom: 26,
             right: 16,
             child: FloatingActionButton(
               backgroundColor: Colors.blue,
-              child: Icon(Icons.skip_next, color: Colors.white, size: 30),
+              child: const Icon(Icons.skip_next, color: Colors.white, size: 30),
               onPressed: () {
-                setState(() {
-                  currentScreen = (currentScreen + 1) % widget.maxScreens;
-                });
+                if (currentScreen < maxScreens) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(
+                        maxScreens: maxScreens,
+                        currentScreen: currentScreen + 1,
+                      ),
+                    ),
+                  );
+                }
               },
             ),
           ),
           Positioned(
             top: 26,
-            left: MediaQuery.of(context).size.width / 2 - 10, // Center horizontally
+            left: MediaQuery.of(context).size.width / 2 - 10,
             child: Text(
-              '${currentScreen+1}',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold
-              ),
-
+              '$currentScreen',
+              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
           ),
         ],
